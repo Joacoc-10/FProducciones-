b7348df9fa5aadf2a72f17645aaf44c8bb04a29c
Joaco Curbelo <joacoc-10@hotmail.com>
2025-11-29 14:51:36 +0100

    Mejora en rendimineto del modal

--- src/app/@modal/(...)events/[slug]/page.tsx
+++ src/app/@modal/(...)events/[slug]/page.tsx
@@ -13,12 +13,5 @@ export default async function EventModal({ params }: EventModalProps) {
 
   if (!event) return null;
 
-  return (
-    <div>
-      <div>
-        <h1>{event.title}</h1>
-        <p>{event.fullDescription}</p>
-      </div>
-    </div>
-  );
+  return null;
 }
